072bebf0646d27c2fbf0054a7e0d603d9d5fb84f
Ibirn <kmcricciuti@gmail.com>
2020-08-28 18:00:27 +0000

    completed tutorial on making this object oriented with both states and behaviours.

--- index.js
+++ index.js
@@ -8,28 +8,38 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+    markComplete: function() {
+      this.complete = true;
+    },
   }
   return task;
 }
+
+// Print the state of a task to the console in a nice readable way
+// function logTaskState(task) {
+
+// }
+
+// function completeTask(task) {
+
+// }
+
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-console.log(tasks)
+task1.logState()
+task1.markComplete()
+task1.logState()
 
 // // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-// function completeTask(taskIndex) {
-//   taskComplete[taskIndex] = true;
-// }
 
-// // Print the state of a task to the console in a nice readable way
-// function logTaskState(taskIndex) {
-//   const title = taskTitles[taskIndex];
-//   const complete = taskComplete[taskIndex];
-//   console.log(`${title} has${complete ? " " : " not "}been completed`);
-// }
+
 
 // // DRIVER CODE BELOW
 
